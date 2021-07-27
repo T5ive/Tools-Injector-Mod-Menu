8a2af3fd87c08efb83ace26e7b1259ae3561fa9b
TFive <t5_@outlook.co.th>
2021-07-27 18:39:25 +0700

    Update UpdateService.cs

--- Tools Injector Mod Menu/Utility/UpdateService.cs
+++ Tools Injector Mod Menu/Utility/UpdateService.cs
@@ -11,7 +11,7 @@ namespace Tools_Injector_Mod_Menu
         public static async Task CheckGitHubNewerVersion()
         {
             var client = new GitHubClient(new ProductHeaderValue("Tools-Injector-Mod-Menu"));
-            var releases = await client.Repository.Release.GetAll("T5ive", "Tools-Injector-Mod-Menu");
+            var releases = await client.Repository.Release.GetAll("T5ive", "Tools-Injector-Mod-Menu").ConfigureAwait(false);
 
             var latestGitHubVersion = new Version(releases[0].TagName);
             var localVersion = Assembly.GetEntryAssembly()?.GetName().Version;
@@ -19,14 +19,18 @@ namespace Tools_Injector_Mod_Menu
             if (localVersion < latestGitHubVersion)
             {
                 var url = releases[0].Assets[0].BrowserDownloadUrl;
-                if (MyMessage.MsgOkCancel($"You are using version {localVersion}\nThe latest version is {latestGitHubVersion}\nDo you want to update? "))
+                if (MyMessage.MsgOkCancel($"You are using version {localVersion}\n" +
+                                          $"The latest version is {latestGitHubVersion}\n\n" +
+                                          "Do you want to update? "))
                 {
                     Process.Start(url);
                 }
             }
             else
             {
-                MyMessage.MsgShowInfo("You are using the latest version");
+                MyMessage.MsgShowInfo($"You are using version {localVersion}\n" +
+                                      $"The latest version is {latestGitHubVersion }\n\n" +
+                                      "You are using the latest version");
             }
         }
     }
